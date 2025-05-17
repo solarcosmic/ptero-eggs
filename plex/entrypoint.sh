0268ed966e7a30458e2692e8ddc914c76ae55f0c
Solar <abe@solarcosmic.net>
2025-05-17 18:52:59 +1200

    ArchiSteamFarm

--- plex/entrypoint.sh
+++ plex/entrypoint.sh
@@ -2,7 +2,7 @@
 cd /home/container
 
 # Output Current Java Version
-neofetch ## only really needed to show what version is being used. Should be changed for different applications
+# neofetch ## only really needed to show what version is being used. Should be changed for different applications
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
