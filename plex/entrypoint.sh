9aadf1c638bb6342dbc5ec1d6e076a7e00c13046
Solar <abe@solarcosmic.net>
2025-05-17 20:57:35 +1200

    Nevermind

--- plex/entrypoint.sh
+++ plex/entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 cd /home/container
 
+curl -L -o ASF-linux-x64.zip https://github.com/JustArchiNET/ArchiSteamFarm/releases/latest/download/ASF-linux-x64.zip
+if [ ! -f ASF-linux-x64.zip ]; then
+    echo "Download failed: ASF-linux-x64.zip not found."
+    exit 1
+fi
+unzip -o ASF-linux-x64.zip
+if [ ! -f ArchiSteamFarm ]; then
+    echo "Error: ArchiSteamFarm binary executable was not found."
+    exit 1
+fi
+chmod +x ArchiSteamFarm
+clear
+mkdir -p config
+echo "{\"Kestrel\":{\"Endpoints\":{\"HTTP\":{\"Url\":\"http://*:${SERVER_PORT}\"}}}}" > config/IPC.config
+echo "IPC.config created with port ${IPC_PORT}"
+echo "ArchiSteamFarm is now ready to run."
+
 MODIFIED_STARTUP=$(eval echo "${STARTUP//\{\{/\${}")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
