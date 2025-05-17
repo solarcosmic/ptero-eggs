ae3f9a894d2c9dc604e0a7812b06097ff9025904
Solar <abe@solarcosmic.net>
2025-05-17 19:29:43 +1200

    Update entrypoint.sh

--- plex/entrypoint.sh
+++ plex/entrypoint.sh
@@ -7,12 +7,12 @@ if [ ! -f ASF-linux-x64.zip ]; then
     exit 1
 fi
 unzip -o ASF-linux-x64.zip
-if [ ! -f ArchiSteamFarm-Service.sh ]; then
-    echo "Error: ArchiSteamFarm's service was not found."
+if [ ! -f ArchiSteamFarm ]; then
+    echo "Error: ArchiSteamFarm binary executable was not found."
     exit 1
 fi
-chmod +x ArchiSteamFarm-Service.sh
-./ArchiSteamFarm-Service.sh
+chmod +x ArchiSteamFarm
+./ArchiSteamFarm
 
 MODIFIED_STARTUP=$(eval echo "${STARTUP//\{\{/\${}")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
