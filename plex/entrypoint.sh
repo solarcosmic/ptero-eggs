e97186f3be0313469470774f248fb46cb729a839
Solar <abe@solarcosmic.net>
2025-05-17 18:32:59 +1200

    Change to Defaults

--- plex/entrypoint.sh
+++ plex/entrypoint.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
+cd /home/container
 
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
+# Output Current Java Version
+java -version ## only really needed to show what version is being used. Should be changed for different applications
 
-# Set environment variable that holds the Internal Docker IP
-#INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-#export INTERNAL_IP
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Switch to the container's working directory
-cd /home/container || exit 1
-
-# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
-# variable format of "${VARIABLE}" before evaluating the string and automatically
-# replacing the values.
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-echo -e "\033[1m\033[33mcontainer@pterodactyl~ \033[0m$PARSED"
-eval "$PARSED"
+# Run the Server
+${MODIFIED_STARTUP}
