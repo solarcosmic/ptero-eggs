237f5f94f367cb948d39876291e982f4551303b7
Solar <abe@solarcosmic.net>
2025-05-17 16:29:49 +1200

    Update entrypoint.sh

--- plex/entrypoint.sh
+++ plex/entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-cd /home/container
 
-# Replace startup variables
-MODIFIED_STARTUP=$(eval echo "$STARTUP")
-echo ":/home/container$ $MODIFIED_STARTUP"
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
 
-# Execute the startup command
-exec $MODIFIED_STARTUP
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+echo -e "\033[1m\033[33mcontainer@pterodactyl~ \033[0m$PARSED"
+eval "$PARSED"
