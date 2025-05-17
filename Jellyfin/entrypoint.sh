7258ed1dd037dc7e64c53afeef0c84dac96ce043
Solar <abe@solarcosmic.net>
2025-05-17 22:19:30 +1200

    Update

--- Jellyfin/entrypoint.sh
+++ Jellyfin/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/container
 
-curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
+curl https://repo.jellyfin.org/install-debuntu.sh | bash
 
 MODIFIED_STARTUP=$(eval echo "${STARTUP//\{\{/\${}")
 echo ":/home/container$ ${MODIFIED_STARTUP}"
